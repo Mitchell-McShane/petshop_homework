80f625b8ef6f15341149509e0067ebb01d3e8f45
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2020-11-06 17:21:18 +0000

    fourth and fifth task complete

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,13 +1,21 @@
 # WRITE YOUR FUNCTIONS HERE
 
-# Returns the name of the pet shop.
+# Returns the name of the pet shop function.
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
 
-# Gives the total cash of the pet shop.
+# Gives the total cash of the pet shop function.
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
 
-# Adds and removes cash in the pet shop.
+# Adds and removes cash from the pet shop function.
 def add_or_remove_cash(pet_shop, cash):
     pet_shop["admin"]["total_cash"] += cash
+
+# How many pets sold from the pet shop function.
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+# Increase number of pets sold from the pet shop function.
+def increase_pets_sold(pet_shop, pets):
+    pet_shop["admin"]["pets_sold"] += pets
