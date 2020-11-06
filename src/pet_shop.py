819756e81321e47e2dd242e8f60839502e48f264
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2020-11-06 16:42:00 +0000

    completed third task

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -4,5 +4,10 @@
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
 
+# Gives the total cash of the pet shop.
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
+
+# Adds and removes cash in the pet shop.
+def add_or_remove_cash(pet_shop, cash):
+    pet_shop["admin"]["total_cash"] += cash
